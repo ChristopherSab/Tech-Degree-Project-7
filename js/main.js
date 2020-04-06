214b81f158dddfbe6af8eefbf229ddacc0d37b4e
ChristopherSab <chris.sabaoth@protonmail.com>
2020-04-06 20:19:38 +0100

    Added Traffic line Chart

--- js/main.js
+++ js/main.js
@@ -17,4 +17,72 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
+//Traffic Data//
+let trafficChart = document.getElementById("traffic-chart").getContext('2d');
+
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    lineTension: 0,
+    animation: {
+    duration: 0
+    },
+    scales: {
+    yAxes: [{
+    ticks: {
+    beginAtZero:true
+    }
+    }]
+    },
+    legend : {
+    display: false
+    }
+    };
+
+//Global Default Options
+//cityTrafficData.defaults.global.defaultFontFamily = 'Lato',
+
+
+let cityTrafficData = new Chart(trafficChart, {
+    type: 'line', //types of chart could be: bar, horizontal bar, doughnut, pie, radar ...
+
+    data: {
+        labels: ['Leeds', 'Sheffield', 'Manchester', 'Birmingham', 'London', 'Swansea', 'Newcastle',
+        'Liverpool', 'Bradford', 'Wolverhampton', 'Nottingham'],
+
+        datasets: [{
+            data: [
+                4551,
+                6853,
+                3955,
+                7843,
+                9556,
+                3003,
+                1923,
+                8641,
+                2993,
+                2527,
+                7299
+            ],
+            
+            lineTension: 0,
+            pointBackgroundColor: 'white',
+            pointBorderColor: '#7580BF',
+            pointBorderWidth: 3,
+            pointRadius: 5,
+
+            
+        }]
+    },
+
+    options: {
+         
+        legend: {
+            display: false
+        }
+
+    }
+    
+
+});
 
